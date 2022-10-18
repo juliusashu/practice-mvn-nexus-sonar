b042ff9a3630e0725d580346d8950ceb60f7b8b1
unknown <julius.ashu@gmail.com>
2022-10-18 19:35:08 -0400

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		String name = "julius";
+
+		System.out.println(name);
+
 	}
 
 }

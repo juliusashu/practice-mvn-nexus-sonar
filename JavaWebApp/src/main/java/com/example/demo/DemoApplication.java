e879c36f613a21ed1258f448a633062044a58c4e
unknown <julius.ashu@gmail.com>
2022-10-25 21:50:27 -0400

    fix error

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,11 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		String name = "julius";
-
-		System.out.println(name);
-
-		Integer age = "20";
-		System.out.println(age + 5);
-
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "julius";
+        System.out.println(name);
+    }
 }
